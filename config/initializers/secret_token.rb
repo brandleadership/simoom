e25d75e6ea701a30f40b6f540325b44e3a301e59
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-08-17 11:29:35 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simoom::Application.config.secret_token = '27944bc2f0b3f184e1ebfec4130f2894d4868cd75b4777b09bb6d0eaad9d45afb4562b76cf9cbd5cef48fa0a2f26419628cdd0e919ec30cb692094f7f094227f'
