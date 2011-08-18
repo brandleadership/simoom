b3473a2f645b47bd34bd7723756354de94126417
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-08-18 08:26:11 +0200

    rename cassete for complete import

--- spec/models/basecamp/importer_spec.rb
+++ spec/models/basecamp/importer_spec.rb
@@ -5,7 +5,7 @@ describe Basecamp::Importer do
   describe '.perform' do
 
     it 'syncs the relevant data' do
-      VCR.use_cassette('projects', :record => :new_episodes) do
+      VCR.use_cassette('projects_tree', :record => :new_episodes) do
         silence(:stdout) { Basecamp::Importer.perform }
       end
 
