25623b4ecb347734ece93a9f60cf5caec16202b2
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-09-13 16:13:40 +0200

    add some more styling

--- app/controllers/weeks_todo_lists_controller.rb
+++ app/controllers/weeks_todo_lists_controller.rb
@@ -2,8 +2,8 @@ class WeeksTodoListsController < ApplicationController
   respond_to :js
   
   def create
-    @week = Week.find(params[:week_todo_list][:week_id])
-    @todo_list = TodoList.find(params[:week_todo_list][:todo_list_id])
+    @week = Week.find(params[:week_id])
+    @todo_list = TodoList.find(params[:todo_list_id])
     
     @week_todo_list = WeekTodoList.find_or_create_by_week_id_and_todo_list_id(
       @week.id,
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def render_todo_lists collection, action, week = nil
+    week ||= @week
+    render :partial => :todo_list, 
+      :collection => collection,
+      :locals => {
+        :action => action,
+        :week => week
+      }
+  end
 end
