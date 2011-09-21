21e19c5a91177d659d493775e909cb1d04b837ab
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-09-21 16:14:36 +0200

    eliminate trailing whitespace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
-  
+
   def render_todo_lists collection, action, week = nil
     week ||= @week
-    render :partial => :todo_list, 
+    render :partial => :todo_list,
       :collection => collection,
       :locals => {
         :action => action,
         :week => week
       }
   end
+
 end
