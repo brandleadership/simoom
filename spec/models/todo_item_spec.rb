62997c1f94eb063c9f4463e056a6b3710e0a97c3
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-09-26 13:40:43 +0200

    spec complete_in_basecamp

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe TodoItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#complete_in_basecamp' do
+    it 'marks the todo_lists items as complete in basecamp' do
+      basecamp_todo_double = mock_model(Basecamp::TodoItem, :save => true)
+      todo_list = Factory(:todo_list)
+      todo_list.todo_items << TodoItem.new(:basecamp_id => 1)
+      todo_list.should_receive(:fetch_items).and_return([basecamp_todo_double])
+      basecamp_todo_double.should_receive(:completed=).with(true)
+      todo_list.complete_in_basecamp
+    end
+  end
+
 end
